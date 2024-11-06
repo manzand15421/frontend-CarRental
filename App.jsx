3300a4191fd7d0719331c728b7e3462d078754d0
manzx32 <muhsalman54118@gmail.com>
2024-11-06 17:36:01 +0700

    Home Page

--- App.jsx
+++ App.jsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import {
+  FlatList,
   Image,
   SafeAreaView,
   ScrollView,
@@ -15,20 +16,51 @@ import {
   Text,
   useColorScheme,
   View,
-  TouchableOpacity,
 } from 'react-native';
 
 import Button from './src/components/Button';
+import Icon from 'react-native-vector-icons/Feather';
+import CarList from './src/components/Carlist';
 
-const Colors = {
+const Colors = { 
   primary: '#A43333',
   secondary: '#SCB85F',
   darker: '#121212',
   lighter: '#ffffff',
-  button: "#5CB85F",
+  button: '#5CB85F',
 };
 
-function App(): React.JSX.Element {
+const CARS = [
+  {
+    name: 'Innova Zenix',
+    image:
+      'https://medias.auto2000.co.id/sys-master-hybrismedia/h1b/h8a/8846828240926/Thumbnail_Black_Zenix.png',
+    price: 230000,
+  },
+  {
+    name: 'Yaris',
+    image:
+      'https://medias.auto2000.co.id/sys-master-hybrismedia/h05/h86/8846786625566/4-black-+-super-white-ii_optimized.png',
+    price: 150000,
+  },
+  {
+    name: 'Yaris',
+    image:
+      'https://medias.auto2000.co.id/sys-master-hybrismedia/h05/h86/8846786625566/4-black-+-super-white-ii_optimized.png',
+    price: 150000,
+  },
+];
+
+const ButtonIcon = ({icon, title}) => (
+  <Button style={styles.buttonIcon}>
+    <View style={styles.iconWrapper}>
+      <Icon name={icon} size={25} color="#fff" />
+    </View>
+    <Text style={styles.iconText}>{title}</Text>
+  </Button>
+);
+
+function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -44,49 +76,60 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <View style={styles.header}
-          >
-        <View style={styles.headerContainer}>
-          <View>
-            <Text style={{ ...styles.headerText }}>Hi,Dias Hewan</Text>
-            <Text style={{ ...styles.headerText, fontSize: 20 }}>
-              From Tegal Company
-            </Text>
+        <View style={styles.header}>
+          <View style={styles.headerContainer}>
+            <View>
+              <Text style={{...styles.headerText}}>Hi,Dias Hewan</Text>
+              <Text style={{...styles.headerText, fontSize: 20}}>
+                From Tegal Company
+              </Text>
+            </View>
+
+            <View>
+              <Image
+                source={{uri: 'https://i.pravatar.cc/100'}}
+                width={45}
+                height={45}
+                style={styles.imageSetting}
+              />
+            </View>
           </View>
-
-          <View>
-            <Image
-              source={{ uri: 'https://i.pravatar.cc/100' }}
-              width={45}
-              height={45}
-              style={styles.imageSetting}
-            />
-          </View>
-
-
-        </View>
         </View>
         <View style={styles.contentContainer}>
           <View style={styles.wrapper}>
             <Text style={styles.contentText}>
               Sewa Mobil Berkualitas di kawasanmu
             </Text>
-            <Button
-              color={Colors.button}
-              title="Sewa Mobil"
-
-            />
-
+            <Button color={Colors.button} title="Sewa Mobil" />
           </View>
-
+      
           <Image
             source={require('./assets/img_car.png')}
             style={styles.imageCar}
           />
-
+          
         </View>
-
-
+        <View style={styles.iconContainer}>
+          <ButtonIcon icon="truck" title="Sewa Mobil"/>
+          <ButtonIcon icon="box" title="Oleh-Oleh"/>
+          <ButtonIcon icon="key" title="Penginapan"/>
+          <ButtonIcon icon="camera" title="Wisata"/>
+        </View>
+        {/* end banner */}
+        <FlatList
+          data={CARS}
+          renderItem={({item}) => 
+            <CarList
+              key={item.id}
+              image={{ uri: item.image }}
+              carName={item.name}
+              passengers={5}
+              baggage={4}
+              price={item.price}
+            />
+          }
+          keyExtractor={item => item.id}
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -99,11 +142,11 @@ const styles = StyleSheet.create({
   },
 
   headerContainer: {
-  display : 'flex',
+    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 15,
-    alignItems : "center",
+    alignItems: 'center',
   },
 
   headerText: {
@@ -118,12 +161,12 @@ const styles = StyleSheet.create({
   contentContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: "#AF392F",
+    backgroundColor: '#AF392F',
     margin: 10,
     marginTop: -76,
     borderRadius: 10,
-    alignItems: "center",
-    height: 160
+    alignItems: 'center',
+    height: 160,
   },
 
   contentText: {
@@ -136,12 +179,31 @@ const styles = StyleSheet.create({
   wrapper: {
     width: '40%',
     paddingHorizontal: 10,
-
   },
   imageCar: {
-    width: "60%", 
-    marginTop: 34, 
-    borderRadius: 10
+  width: '60%',
+   height : '100%',
+   borderRadius : 10,
+  },
+
+  iconContainer: {
+    marginTop: 75,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap'
+  },
+  iconWrapper: {
+    backgroundColor: Colors.primary,
+    borderRadius:5,
+    padding: 15
+  },
+  iconText:{
+    color:'#fff',
+    fontSize: 12,
+    fontWeight: 700,
+    minWidth: 65,
+    marginTop: 5,
+    textAlign:'center'
   }
   // ButtonStyle : {
   //   marginTop : 10,
@@ -162,6 +224,5 @@ const styles = StyleSheet.create({
 
   // }
 });
-
 
 export default App;
---
+++ src/utils/formafCurrency.js
@@ -0,0 +1,4 @@
+export const formatCurrency = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
